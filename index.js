304c9dc174741878bf3e066777edfe4b0e83e3e8
zacharyrote <zachary.rote@gmail.com>
2017-01-27 11:48:44 -0500

    Middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+const ServiceError = require('core-server').ServiceError;
 
 class AuthCheckMiddleware {
   constructor() {
@@ -7,7 +8,26 @@ class AuthCheckMiddleware {
 
   authCheck(app) {
     return function(req, res, next) {
-      next();
+      if(app.proxy) {
+        app.proxy.apiBindingForServiceType("SecurityService").then((service) => {
+          service.api.sercurity.check({access_token: "111111"}, (validity) => {
+            if(validity.isValid === true) {
+              next();
+            } else {
+              // 403 - Forbidden
+              new ServiceError(HttpStatus.FORBIDDEN, "Forbidden Access").writeResponse(res);
+            }
+          }, (err) => {
+            new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error").writeResponse(res);
+          });
+        }).catch((err) => {
+          // 500 - Internal Service Error
+          new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error").writeResponse(res);
+        });
+      } else {
+        // 503 - Service Unavailable
+        new ServiceError(HttpStatus.SERVICE_UNAVAILABLE, "Security Service Unavailable").writeResponse(res);
+      }
     }
   }
 }
