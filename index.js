3f5878ee9d26b4b1685d254c4df80c0023d7184b
zacharyrote <zachary.rote@gmail.com>
2017-01-27 13:15:53 -0500

    fix auth

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 'use strict';
-const ServiceError = require('core-server').ServiceError;
+const HttpStatus = require('http-status');
 
 class AuthCheckMiddleware {
-  constructor() {
+  constructor(app) {
+    this.app = app;
   }
 
 
-  authCheck(app) {
+  authCheck() {
+    let self = this;
     return function(req, res, next) {
-      if(app.proxy) {
-        app.proxy.apiForServiceType("SecurityService").then((service) => {
-          service.api.sercurity.check({access_token: "111111"}, (validity) => {
-            if(validity.isValid === true) {
-              next();
-            } else {
-              // 403 - Forbidden
-              new ServiceError(HttpStatus.FORBIDDEN, "Forbidden Access").writeResponse(res);
-            }
-          }, (err) => {
-            new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error").writeResponse(res);
-          });
+      if(self.app.proxy) {
+        self.app.proxy.apiForServiceType("SecurityService").then((service) => {
+          console.log(service);
+          next();
+          // service.api.sercurity.check({access_token: "111111"}, (validity) => {
+          //   if(validity.isValid === true) {
+          //     next();
+          //   } else {
+          //     // 403 - Forbidden
+          //     new ServiceError(HttpStatus.FORBIDDEN, "Forbidden Access").writeResponse(res);
+          //   }
+          // }, (err) => {
+          //   new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error").writeResponse(res);
+          // });
         }).catch((err) => {
           // 500 - Internal Service Error
-          new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error").writeResponse(res);
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ errorMessage: "Internal Server Error" });
         });
       } else {
         // 503 - Service Unavailable
-        new ServiceError(HttpStatus.SERVICE_UNAVAILABLE, "Security Service Unavailable").writeResponse(res);
+        res.status(HttpStatus.SERVICE_UNAVAILABLE).send({ errorMessage: "Security Service Unavailable" });
       }
     }
   }
