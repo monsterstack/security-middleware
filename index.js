fc39237566c9adca5970b55f18ce9ea0b85298fa
zacharyrote <zachary.rote@gmail.com>
2017-01-30 13:29:50 -0500

    deal with case where service is not found

--- index.js
+++ index.js
@@ -14,23 +14,27 @@ class AuthCheckMiddleware {
       if(accessToken === undefined) {
         res.status(HttpStatus.BAD_REQUEST).send({errorMessage: "Bad Request, Missing access-token"});
       } else if(self.app.proxy) {
-        self.app.proxy.apiForServiceType("SecurityService").then((service) => {
-          service.api.tokens.check({'access-token': accessToken}, (validity) => {
-            if(validity.valid === true) {
-              next();
-            } else {
-              // 403 - Forbidden
-              res.status(HttpStatus.FORBIDDEN).send({errorMessage: "Forbidden Access"});
-            }
-          }, (err) => {
-            if(err.status === HttpStatus.NOT_FOUND) {
-              res.status(HttpStatus.UNAUTHORIZED).send({ errorMessage: "Unauthorized for BEARER, Matching Access Token Not Found" });
-            } else if(err.status === HttpStatus.BAD_REQUEST) {
-              res.status(err.status).send({ errorMessage: "Bad Request" });
-            } else {
-              res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({errorMessage: "Internal Server Error"});
-            }
-          });
+          self.app.proxy.apiForServiceType("SecurityService").then((service) => {
+          if(service) {
+            service.api.tokens.check({'access-token': accessToken}, (validity) => {
+              if(validity.valid === true) {
+                next();
+              } else {
+                // 403 - Forbidden
+                res.status(HttpStatus.FORBIDDEN).send({errorMessage: "Forbidden Access"});
+              }
+            }, (err) => {
+              if(err.status === HttpStatus.NOT_FOUND) {
+                res.status(HttpStatus.UNAUTHORIZED).send({ errorMessage: "Unauthorized for BEARER, Matching Access Token Not Found" });
+              } else if(err.status === HttpStatus.BAD_REQUEST) {
+                res.status(err.status).send({ errorMessage: "Bad Request" });
+              } else {
+                res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({errorMessage: "Internal Server Error"});
+              }
+            });
+          } else {
+            res.status(HttpStatus.SERVICE_UNAVAILABLE).send({errorMessage: "Security Service Unavailable!"});
+          }
         }).catch((err) => {
           // 500 - Internal Service Error
           console.log(err);
