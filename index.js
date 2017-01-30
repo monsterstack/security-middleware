0ab995460d94ff12f1d3afc063034869cb64656f
zacharyrote <zachary.rote@gmail.com>
2017-01-30 12:37:24 -0500

    auth check middleware

--- index.js
+++ index.js
@@ -10,20 +10,19 @@ class AuthCheckMiddleware {
   authCheck() {
     let self = this;
     return function(req, res, next) {
+      let accessToken = "1111111"; // Get from Authorization Header;
       if(self.app.proxy) {
         self.app.proxy.apiForServiceType("SecurityService").then((service) => {
-          console.log(service);
-          next();
-          // service.api.sercurity.check({access_token: "111111"}, (validity) => {
-          //   if(validity.isValid === true) {
-          //     next();
-          //   } else {
-          //     // 403 - Forbidden
-          //     new ServiceError(HttpStatus.FORBIDDEN, "Forbidden Access").writeResponse(res);
-          //   }
-          // }, (err) => {
-          //   new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error").writeResponse(res);
-          // });
+          service.api.tokens.check({access_token: "111111"}, (validity) => {
+            if(validity.valid === true) {
+              next();
+            } else {
+              // 403 - Forbidden
+              new ServiceError(HttpStatus.FORBIDDEN, "Forbidden Access").writeResponse(res);
+            }
+          }, (err) => {
+            new ServiceError(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error").writeResponse(res);
+          });
         }).catch((err) => {
           // 500 - Internal Service Error
           res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({ errorMessage: "Internal Server Error" });
